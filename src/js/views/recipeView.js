fe41d7464a87af25ad9848257de8fc8d896ad78b
IvanYCSung <syc0615@hotmail.com>
2022-10-29 20:05:53 +1100

    fraction issue

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,6 +2,8 @@ import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 import View from './View.js';
 
+const Fraction = require('fractional').Fraction;
+
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
